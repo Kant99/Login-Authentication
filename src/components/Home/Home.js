29faa694c046064929053a702ce91ccfd17b1eb7
Kant99 <reshukumar123@gmail.com>
2023-12-06 11:27:57 +0530

    Logout button added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../../firebase';
+import { signOut } from 'firebase/auth';
 
 const Home = (props) => {
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
   return (
     <div>
-    <div>
-       <h1> <Link to="./login" >Login</Link></h1><br/>
-       <h1> <Link to="./signup" >Signup</Link></h1>
-    </div>
-    <br/>
-    <br/>
-    <br/>
+      <div>
+        <h1>
+          <Link to="./login">Login</Link>
+        </h1>
+        <br />
+        <h1>
+          <Link to="./signup">Signup</Link>
+        </h1>
+      </div>
+      <br />
+      <br />
+      <br />
 
-    <h2>{props.name ? `Welcome - ${props.name}`:"Login Please"}   </h2>
+      {props.name ? (
+        <>
+          <h2>{`Welcome - ${props.name}`}</h2>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <h2>Login Please</h2>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
